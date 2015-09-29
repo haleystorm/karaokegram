c79b2e98cdd0921250796ddde4b61d2ba625a5f0
haleystorm <haley.l.boyd@gmail.com>
2015-09-29 12:59:04 -0700

    User can now watch video from search.

--- public/js/karaokegram.js
+++ public/js/karaokegram.js
@@ -9,11 +9,18 @@
 
         // Search button click
         $('#btnDoSearch').on('click', doSearch);
+
+        // Show video link click. Must be done on static element.
+        $('#resultList ul').on('click', 'li div a.linkshowvideo', showVideo);
     });
 
+    // Functions ================================
+
+    // Fill list with data
     function doSearch(event) {
         event.preventDefault();
 
+        // Empty content string
         var tableContent = '';
 
         var q = $('#searchYouTube fieldset input#inputSearch').val();
@@ -38,6 +45,25 @@
             // Inject the whole content string into our existing HTML table
             $('#resultList ul').html(tableContent);
         });
+    }
+
+    // Show YouTube Video
+    function showVideo(event) {
+
+        // Prevent link from firing
+        event.preventDefault();
+
+        // Retrieve video id from the link rel attribute
+        var thisId = $(this).attr('rel');
+
+        // Create url for video with thisId
+        var url = "https://www.youtube.com/embed/" + thisId;
+
+        // Create iframe for video
+        var videoContent = '<iframe class="frameYouTube" scrolling="no" frameborder="no" src="' + url + '" ></iframe>';
+
+        // Inject the video content string into our exiting HTML div
+        $('#watchYouTube').html(videoContent);
     }
 
 })();
